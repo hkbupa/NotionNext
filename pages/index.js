339c57bbd147ecbe8a4788d7dce2de233e81fc5b
hkbupa <admin@hkbupa.org>
2025-02-24 09:57:44 +0800

    Update index.js
    修改為全部在新標籤頁打開

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import BLOG from '@/blog.config'
-import { siteConfig } from '@/lib/config'
-import { getGlobalData, getPostBlocks } from '@/lib/db/getSiteData'
-import { generateRobotsTxt } from '@/lib/robots.txt'
-import { generateRss } from '@/lib/rss'
-import { generateSitemapXml } from '@/lib/sitemap.xml'
-import { DynamicLayout } from '@/themes/theme'
-import { generateRedirectJson } from '@/lib/redirect'
+import { useEffect } from 'react';
+import BLOG from '@/blog.config';
+import { siteConfig } from '@/lib/config';
+import { getGlobalData, getPostBlocks } from '@/lib/db/getSiteData';
+import { generateRobotsTxt } from '@/lib/robots.txt';
+import { generateRss } from '@/lib/rss';
+import { generateSitemapXml } from '@/lib/sitemap.xml';
+import { DynamicLayout } from '@/themes/theme';
+import { generateRedirectJson } from '@/lib/redirect';
 
 /**
  * 首页布局
@@ -13,26 +14,33 @@ import { generateRedirectJson } from '@/lib/redirect'
  * @returns
  */
 const Index = props => {
-  const theme = siteConfig('THEME', BLOG.THEME, props.NOTION_CONFIG)
-  return <DynamicLayout theme={theme} layoutName='LayoutIndex' {...props} />
-}
+  const theme = siteConfig('THEME', BLOG.THEME, props.NOTION_CONFIG);
+
+  useEffect(() => {
+    document.querySelectorAll('a').forEach(link => {
+      link.setAttribute('target', '_blank');
+    });
+  }, []);
+
+  return <DynamicLayout theme={theme} layoutName='LayoutIndex' {...props} />;
+};
 
 /**
  * SSG 获取数据
  * @returns
  */
 export async function getStaticProps(req) {
-  const { locale } = req
-  const from = 'index'
-  const props = await getGlobalData({ from, locale })
+  const { locale } = req;
+  const from = 'index';
+  const props = await getGlobalData({ from, locale });
   const POST_PREVIEW_LINES = siteConfig(
     'POST_PREVIEW_LINES',
     12,
     props?.NOTION_CONFIG
-  )
+  );
   props.posts = props.allPages?.filter(
     page => page.type === 'Post' && page.status === 'Published'
-  )
+  );
 
   // 处理分页
   if (siteConfig('POST_LIST_STYLE') === 'scroll') {
@@ -41,34 +49,34 @@ export async function getStaticProps(req) {
     props.posts = props.posts?.slice(
       0,
       siteConfig('POSTS_PER_PAGE', 12, props?.NOTION_CONFIG)
-    )
+    );
   }
 
   // 预览文章内容
   if (siteConfig('POST_LIST_PREVIEW', false, props?.NOTION_CONFIG)) {
     for (const i in props.posts) {
-      const post = props.posts[i]
+      const post = props.posts[i];
       if (post.password && post.password !== '') {
-        continue
+        continue;
       }
-      post.blockMap = await getPostBlocks(post.id, 'slug', POST_PREVIEW_LINES)
+      post.blockMap = await getPostBlocks(post.id, 'slug', POST_PREVIEW_LINES);
     }
   }
 
   // 生成robotTxt
-  generateRobotsTxt(props)
+  generateRobotsTxt(props);
   // 生成Feed订阅
-  generateRss(props)
+  generateRss(props);
   // 生成
-  generateSitemapXml(props)
+  generateSitemapXml(props);
   if (siteConfig('UUID_REDIRECT', false, props?.NOTION_CONFIG)) {
     // 生成重定向 JSON
-    generateRedirectJson(props)
+    generateRedirectJson(props);
   }
 
   // 生成全文索引 - 仅在 yarn build 时执行 && process.env.npm_lifecycle_event === 'build'
 
-  delete props.allPages
+  delete props.allPages;
 
   return {
     props,
@@ -79,7 +87,7 @@ export async function getStaticProps(req) {
           BLOG.NEXT_REVALIDATE_SECOND,
           props.NOTION_CONFIG
         )
-  }
+  };
 }
 
-export default Index
+export default Index;
